d8836717311f024012ce8a85aa09413e37f6a9f4
HamzaDmitriyevich <krou1937@gmail.com>
2024-01-29 19:30:05 +0300

    first commit

---
+++ dz.js
@@ -0,0 +1,23 @@
+class vos{
+    constructor(buttonID){
+        this.button = buttonID;
+        let mybutton = new vos(button-id-vs);
+        
+        this.button.addEventListener(
+            'click',
+            this.voshod.bind(this)
+        );
+
+    }
+    voshod() {
+        this.button.style.transform = 'scale(1.2)';
+            setTimeout(()=>{
+                this.button.style.transform = 'scale(1)';
+            },
+
+            200);
+            console.log('Не трогай меня');
+    }
+    
+}
+
